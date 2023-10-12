016227ed05ad840e657c19eb7b00e2621b752124
Jerry Zhao <jerryzhao16@gmail.com>
2023-10-12 15:35:59 -0400

    added speed up item

--- Assets/CollisionController.cs
+++ Assets/CollisionController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CollisionController : MonoBehaviour
 {
     public Canvas gameOverScreen;
+    private float speedMultiplier = 2;
+    private float boostTimer = -100.0f;
+    private float boostLength = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,16 @@ public class CollisionController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Debug.Log(gameObject.GetComponent<PlayerScript>().speed.x);
+        // Handles boost: Updates the boost timer, and resets player speed when it reaches 0
+        if (boostTimer > -100.0f) {
+            boostTimer -= Time.deltaTime;
+            if (boostTimer <= 0.0f) {
+                boostTimer = -100.0f;
+                gameObject.GetComponent<PlayerScript>().speed.x = gameObject.GetComponent<PlayerScript>().baseSpeed;
+                gameObject.GetComponent<PlayerScript>().speed.y = gameObject.GetComponent<PlayerScript>().baseSpeed / 1.5f;
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision){
@@ -27,7 +39,19 @@ public class CollisionController : MonoBehaviour
             Debug.Log("Hit Wall");
         }
 
-
+        if (collision.gameObject.CompareTag("SpeedUpItem")) {
+            Destroy(collision.gameObject);
+            if (boostTimer > -100.0f) {
+                boostTimer = boostLength;
+            }
+            else {
+                gameObject.GetComponent<PlayerScript>().speed.x *= speedMultiplier;
+                gameObject.GetComponent<PlayerScript>().speed.y *= speedMultiplier;
+                Debug.Log(gameObject.GetComponent<PlayerScript>().speed.x);
+                boostTimer = boostLength;
+            }
+            
+        }
     }
 
 
--- Assets/Player1MoveSmooth.cs
+++ Assets/Player1MoveSmooth.cs
@@ -10,21 +10,27 @@ public class PlayerScript : MonoBehaviour
   /// <summary>
   /// 1 - The speed of the ship
   /// </summary>
-  public Vector2 speed = new Vector2(1, 1);
+
+  public Vector2 speed = new Vector2(12, 8);
+
+  public float baseSpeed = 12;
  
   // 2 - Store the movement
   private Vector2 movement;
+
  
   void Update()
   {
     // 3 - Retrieve axis information
-    float inputX = Input.GetAxis("Horizontal1");
-    float inputY = Input.GetAxis("Vertical1");
+    float inputX = Input.GetAxis("Horizontal");
+    float inputY = Input.GetAxis("Vertical");
  
     // 4 - Movement per direction
     movement = new Vector2(
       speed.x * inputX,
       speed.y * inputY);
+
+    Debug.Log(baseSpeed);
  
   }
  
