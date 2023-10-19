529ee7b7a5e5043ef508664de656b8dd472176e7
Robin <robinl21@mit.edu>
2023-10-19 14:21:44 -0400

    MainMenu Scene

--- Assets/GameOverScreen.cs
+++ Assets/GameOverScreen.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class RestartButton : MonoBehaviour
+public class GameOverScreen : MonoBehaviour
 {   
     public Canvas gameOverScreen;
 
-    public void Update() {
-        Debug.Log("BUTTON RUNNING");
-    }
     public void ResetTheGame() {
         gameOverScreen.gameObject.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Debug.Log("The button is working.");
+    }
+
+    public void ReturnToMainMenu() {
+        gameOverScreen.gameObject.SetActive(false);
+        SceneManager.LoadScene("MenuScene");
+        
     }
 }
--- Assets/StartGame.cs
+++ Assets/StartGame.cs
@@ -6,7 +6,7 @@ public class StartGame : MonoBehaviour
 {
     public void StartTheGame() {
         Debug.Log("Button working");
-        SceneManager.LoadScene("Sample Scene");
+        SceneManager.LoadScene("SampleScene");
         Debug.Log("The button is working.");
     }
 }
